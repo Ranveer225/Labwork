36547ea29eceae793efe827dce081261f6da25a9
Ranveer225 <124071847+Ranveer225@users.noreply.github.com>
2024-08-30 02:55:26 +0530

    Add in attributes for class

--- Labwork1/src/main/java/com/mycompany/labwork1/Vehicle.java
+++ Labwork1/src/main/java/com/mycompany/labwork1/Vehicle.java
@@ -9,6 +9,11 @@ package com.mycompany.labwork1;
  * @author ranveersingh
  */
 public class Vehicle {
+    
+    private int numberOfWheels;
+    private String color;
+    private float engineSize;
+    private String fuelType;
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
