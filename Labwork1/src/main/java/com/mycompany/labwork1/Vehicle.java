43668289888f020ae1cd30d0810f32dae670fba7
Ranveer225 <124071847+Ranveer225@users.noreply.github.com>
2024-08-30 02:58:06 +0530

    Add constructors for vehicles

--- Labwork1/src/main/java/com/mycompany/labwork1/Vehicle.java
+++ Labwork1/src/main/java/com/mycompany/labwork1/Vehicle.java
@@ -10,10 +10,21 @@ package com.mycompany.labwork1;
  */
 public class Vehicle {
     
+    //Attributes of class
+    
     private int numberOfWheels;
     private String color;
     private float engineSize;
     private String fuelType;
+    
+    // Constructors for vehicle
+    
+    public Vehicle(int numberOfWheels, String color, float engineSize, String fuelType) {
+        this.numberOfWheels = numberOfWheels;
+        this.color = color;
+        this.engineSize = engineSize;
+        this.fuelType = fuelType;
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
