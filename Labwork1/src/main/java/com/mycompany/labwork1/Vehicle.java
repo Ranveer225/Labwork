e341ee040f85180d21aad8fec25a3402bdd7675c
Ranveer225 <124071847+Ranveer225@users.noreply.github.com>
2024-08-30 03:10:08 +0530

    Add in getters and setters

--- Labwork1/src/main/java/com/mycompany/labwork1/Vehicle.java
+++ Labwork1/src/main/java/com/mycompany/labwork1/Vehicle.java
@@ -25,8 +25,80 @@ public class Vehicle {
         this.engineSize = engineSize;
         this.fuelType = fuelType;
     }
+    
+    // Getters and setters 
+    public int getNumberOfWheels() {
+        return numberOfWheels;
+    }
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public void setNumberOfWheels(int numberOfWheels) {
+        this.numberOfWheels = numberOfWheels;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public float getEngineSize() {
+        return engineSize;
+    }
+
+    public void setEngineSize(float engineSize) {
+        this.engineSize = engineSize;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
     }
 }
+
+// New class Car to extend Vehicle
+
+public class Car extends Vehicle {
+   
+    private String brand;
+    
+    // Additional attribute
+
+   
+    public Car(int numberOfWheels, String color, float engineSize, String fuelType, String brand) {
+        
+        super(numberOfWheels, color, engineSize, fuelType);
+        this.brand = brand;
+    }
+
+   
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    
+    public void honk() {
+        System.out.println("Honk, honk!");
+    }
+    
+    // Method named honk to print honk, honk
+
+    
+    // Method to print all attributes
+    public void displayInfo() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Number of Wheels: " + getNumberOfWheels());
+        System.out.println("Color: " + getColor());
+        System.out.println("Engine Size: " + getEngineSize() + "L");
+        System.out.println("Fuel Type: " + getFuelType());
+    }
+}
+
